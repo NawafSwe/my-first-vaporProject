a858f574531b470995f0177864c1e48da5a630b8
Nawaf <Nawaf.jr9@hotmial.com>
2020-10-16 01:41:53 +0300

    adding the user controller

--- Sources/App/Migrations/CreateUser.swift
+++ Sources/App/Migrations/CreateUser.swift
@@ -5,4 +5,23 @@
 //  Created by Nawaf B Al sharqi on 16/10/2020.
 //
 
-import Foundation
+import Fluent
+struct CreateUser : Migration{
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        //giving the name of the collection 'users'
+        return database.schema("users")
+            //specifying the schema of the user where user has id , username , password , email
+            .id()
+            .field("username", .string, .required)
+            .field("password", .string, .required)
+            .field("email", .string , .required)
+            .create()
+    }
+    
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        return database.schema("users").delete()
+    }
+    
+    
+}
+
