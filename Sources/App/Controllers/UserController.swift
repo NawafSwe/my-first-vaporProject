1792ddecfa79eec24922f26c3ba837e82a9e24b6
Nawaf <Nawaf.jr9@hotmial.com>
2020-10-16 17:56:45 +0300

    adding userModel and creating first route for users

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -7,6 +7,20 @@
 
 import Fluent
 import Vapor
-
+struct UserController : RouteCollection{
+    func boot(routes: RoutesBuilder) throws {
+        //telling that the route path is /users
+        let users : RoutesBuilder =  routes.grouped("users")
+        //specifying the routes and their functions
+        users.get("users", use: index)
+        
+        
+    }
+    
+    //index for getting all users  --> /users
+    func index(req : Request) throws -> EventLoopFuture<[User]>{
+        return User.query(on: req.db).all()
+    }
+}
 
 
