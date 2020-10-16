4516d99bcfed447031386ae3c6cde81434b7a8bf
Nawaf <Nawaf.jr9@hotmial.com>
2020-10-16 20:33:35 +0300

    update

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Nawaf B Al sharqi on 16/10/2020.
 //
-
 import Fluent
 import Vapor
 struct UserController : RouteCollection{
@@ -13,19 +12,18 @@ struct UserController : RouteCollection{
         let users : RoutesBuilder =  routes.grouped("users")
         //specifying the routes and their functions
         users.get("users", use: index)
-        
-        
+        users.post("users", use: create)
     }
-    
     //index for getting all users  --> /users
     func index(req : Request) throws -> EventLoopFuture<[User]>{
         return User.query(on: req.db).all()
     }
     
+    //creating the user
     func create(req: Request)throws -> EventLoopFuture<User>{
         //decoding the data from json object to UserModel
         let user = try req.content.decode(User.self)
-        //saving the user model into the database and returns its info 
+        //saving the user model into the database and returns its info
         return user.save(on: req.db).map({user})
     }
 }
